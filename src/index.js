cf4ce5566615530dd628e4bfb3bb088d8185a290
antoprokopovich <antoprokopovich@gmail.com>
2019-03-19 08:56:06 +0300

    feat: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let currentNumber = number;
+  let currentBase = base;
+  let multiplier = 2;
+  let count = 0;
+  let multiArr = [];
+  let countArr = [];
+  let result = 0;
+  let resArr = [];
+  let value = 0;
+
+  while ((currentBase != 1) || (count != 0)) {
+    if (currentBase % multiplier == 0) {
+      count++;
+      currentBase /= multiplier;
+    } else {
+      if (count != 0) {
+        multiArr.push(multiplier);
+        countArr.push(count);
+        count = 0;
+        multiplier++;
+      } else {
+        multiplier++;
+      }
+    } 
+  }
+
+  for (i = 0, length = multiArr.length; i < length; i++) {
+    value = multiArr[i];
+    while (currentNumber / value > 1) {
+      result += Math.floor(currentNumber / value);
+      value *= multiArr[i];
+    }
+    resArr.push(Math.floor(result / countArr[i]));
+    result = 0;
+  }
+  
+  resArr.sort(function(a,b) {
+    return a - b;
+  });
+
+  return resArr[0];
 }
